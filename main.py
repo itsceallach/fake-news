ceaef564b55c3f85fc126c7874944ea8d9e6a6cb
itsceallach <51833224+itsceallach@users.noreply.github.com>
2023-06-22 10:15:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -69,4 +69,4 @@ def gfg():
         return render_template('index.html', mydata=answer)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
